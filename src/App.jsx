a139378a4018be296a283c456b1a71671d7ee880
mohit kumawat <mohitkumawat8084@gmail.com>
2026-07-15 09:50:51 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,33 @@ const App = () => {
 
   const [user, setUser] = useState(null)
   const [loggedInUserData, setLoggedInUserData] = useState(null)
-  const [userData,SetUserData] = useContext(AuthContext)
+  const [userData, SetUserData] = useContext(AuthContext)
 
-  useEffect(()=>{
+  useEffect(() => {
     const loggedInUser = localStorage.getItem('loggedInUser')
-    
-    if(loggedInUser){
+
+    if (loggedInUser) {
       const userData = JSON.parse(loggedInUser)
       setUser(userData.role)
       setLoggedInUserData(userData.data)
     }
 
-  },[])
+  }, [])
 
 
   const handleLogin = (email, password) => {
-    if (email == 'admin@me.com' && password == '123') {
+    if (email === "admin@example.com" && password === "123") {
       setUser('admin')
       localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
     } else if (userData) {
-      const employee = userData.find((e) => email == e.email && e.password == password)
+      const employee = userData.find(
+        (e) => email === e.email && password === e.password
+      )
+
       if (employee) {
-        setUser('employee')
-        setLoggedInUserData(employee)
-        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employee',data:employee }))
+        // Login Success
+      } else {
+        alert("Invalid Email or Password")
       }
     }
     else {
@@ -44,7 +47,7 @@ const App = () => {
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ''}
-      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null) }
+      {user == 'admin' ? <AdminDashboard changeUser={setUser} /> : (user == 'employee' ? <EmployeeDashboard changeUser={setUser} data={loggedInUserData} /> : null)}
     </>
   )
 }
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -40,7 +40,7 @@ const Login = ({handleLogin}) => {
                 }}
                 required 
                 className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
-                <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
+                <button  type="submit" className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
             </form>
         </div>
     </div>
